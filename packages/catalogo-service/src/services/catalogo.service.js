a69ff772b442700cfbf1973a7282b1ad4f8e6d67
Jorge Luiz <jorgepaiva1806@gmail.com>
2021-12-10 19:09:41 -0400

    criacao das actions do catalogo.service

--- packages/catalogo-service/src/services/catalogo.service.js
+++ packages/catalogo-service/src/services/catalogo.service.js
@@ -17,6 +17,38 @@ module.exports = {
                 }
                 return false;
             }
+        },
+
+        update: {
+            async handler(ctx){
+                let filme = await Filme.exists({_id: ctx.params.id});
+
+                if(filme){
+                    return await Filme.updateOne({nome: filme.nome, sinopse: filme.sinopse})
+                }
+            }
+        },
+
+
+        exists: {
+            async handler(ctx) {
+               if(ctx.params.id){
+                   return await Filme.exists({_id: ctx.params.id});
+               }
+               return false;
+            }
+        },
+
+        remove:{
+            async handler(ctx){
+                return await Filme.findByIdAndRemove({_id: ctx.params.id});
+            }
+        },
+
+        list: {
+            async handler() {
+                return await Filme.find();
+            }
         }
     }
 }
