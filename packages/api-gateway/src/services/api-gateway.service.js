16b719d1ba070370bb5a6bde1b6b0e85d294095d
Jorge Luiz <jorgepaiva1806@gmail.com>
2021-12-12 02:18:15 -0400

    listando catalogo via api-gateway

--- packages/api-gateway/src/services/api-gateway.service.js
+++ packages/api-gateway/src/services/api-gateway.service.js
@@ -10,6 +10,7 @@ module.exports = {
                 path: "/api",
                 aliases:{
                     "POST /catalogo": "v1.catalogo-service.criar",
+                    "GET /catalogo/": "v1.catalogo-service.listar",
                     "GET /catalogo": "v1.catalogo-service.listar",
                     "PATCH /catalogo": "v1.catalogo-service.atualizar",
                     "DELETE /catalogo/:id": "v1.catalogo-service.excluir",
